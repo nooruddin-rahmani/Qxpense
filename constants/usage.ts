e0729557694dbd70d6596ac0df920f151bf27d90
Nooruddin <nooruddin.rahmani100@gmail.com>
2024-08-08 18:36:30 +0430

    extended usage

--- constants/usage.ts
+++ constants/usage.ts
@@ -1,3 +1,3 @@
-export const basicPlan = { limit: 100, name: 'basic' };
+export const basicPlan = { limit: 10000, name: 'basic' };
 
-export const premiumPlan = { limit: 2000, name: 'premium' };
+export const premiumPlan = { limit: 200000, name: 'premium' };
